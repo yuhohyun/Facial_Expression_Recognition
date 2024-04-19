eff8dbb7b5fbeb3bc59dd2df81e8d6c3947fe03e
Hohyun <yuhohyun@naver.com>
2024-04-19 16:28:10 +0900

    change utils/data_utils.py (data augmentation 추가)

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -7,11 +7,20 @@ from torchvision import transforms
 class CustomImageDataset(Dataset):
     def __init__(self, annotations_file, img_dir, transform=None):
         self.img_labels = pd.read_csv(annotations_file)
-        # 레이블 인코딩 수정
         self.label_encodings = {'positive': 2, 'negative': 0, 'neutral': 1}
         self.img_labels['label'] = self.img_labels['label'].map(self.label_encodings)
         self.img_dir = img_dir
-        self.transform = transform
+        
+        # 데이터 증강을 위한 변환을 추가
+        if transform is None:
+            self.transform = transforms.Compose([
+                transforms.ColorJitter(brightness=0.5, contrast=0.5, saturation=0.5, hue=0.5),
+                transforms.RandomHorizontalFlip(p=0.5),
+                transforms.RandomVerticalFlip(p=0.5),
+                transforms.ToTensor(),
+            ])
+        else:
+            self.transform = transform
 
     def __len__(self):
         return len(self.img_labels)
@@ -19,7 +28,6 @@ class CustomImageDataset(Dataset):
     def __getitem__(self, idx):
         img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0])
         image = Image.open(img_path).convert('RGB')
-        # 수정된 정수형 레이블 사용
         label = self.img_labels.iloc[idx, 1]
         if self.transform:
             image = self.transform(image)
